e900b8e4d80a112fd523cf811e5af7d0a451c32a
Desiree Herschberger <citizendez@gmail.com>
2021-01-03 12:36:37 -0500

    gwsis db model

--- MySQL/Schema.sql
+++ MySQL/Schema.sql
@@ -5,7 +5,9 @@ ID_Course INT NOT NULL,
 Section VARCHAR(20),
 StartDate DATE,
 EndDate DATE,
-PRIMARY KEY(ID_Class)
+PRIMARY KEY(ID_Class),
+FOREIGN KEY class_ID_Course(ID_Course)
+REFERENCES course(ID_Course)
 );
 
 DROP TABLE IF EXISTS course;
@@ -39,4 +41,34 @@ FirstName VARCHAR(50),
 MiddleName VARCHAR(50),
 BirthDate DATE,
 PRIMARY KEY(ID_Staff)
+);
+
+DROP TABLE IF EXISTS classparticipant;
+CREATE table classparticipant(
+ID_Classparticipant INT NOT NULL AUTO_INCREMENT,
+ID_Student INT,
+ID_Class INT,
+StartDate DATE,
+EndDate DATE,
+PRIMARY KEY(ID_Classparticipant),
+FOREIGN KEY cp_ID_Student(ID_Student)
+REFERENCES student(ID_Student),
+FOREIGN KEY cp_ID_Class(ID_Class)
+REFERENCES class(ID_Class)
+);
+
+
+DROP TABLE IF EXISTS staffassignment;
+CREATE table staffassignment(
+ID_Staffassignment INT NOT NULL AUTO_INCREMENT,
+ID_Staff INT,
+ID_Class INT,
+Role VARCHAR(50),
+StartDate DATE,
+EndDate DATE,
+PRIMARY KEY(ID_Staffassignment),
+FOREIGN KEY sa_ID_Staff(ID_Staff)
+REFERENCES staff(ID_Staff),
+FOREIGN KEY sa_ID_Class(ID_Class)
+REFERENCES class(ID_Class)
 );
